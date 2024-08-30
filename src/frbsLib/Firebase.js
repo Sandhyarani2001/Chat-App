121fc0feeadc500401ecb9d846a61a7eb52766be
Sandhyarani <sanvyrani@gmail.com>
2024-08-30 23:38:16 +0530

    add env file

--- src/frbsLib/Firebase.js
+++ src/frbsLib/Firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-import {getFirestore} from "firebase/firestore"
-import {getStorage} from "firebase/storage"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+import conf from "../conf/conf";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6a_h50SvyOzcLaEZ0-HQci3ciJ_Aivko",
-  authDomain: "reactchat-818b8.firebaseapp.com",
-  projectId: "reactchat-818b8",
-  storageBucket: "reactchat-818b8.appspot.com",
-  messagingSenderId: "610253219706",
-  appId: "1:610253219706:web:d0ae48ee8fa9f036bf18c3"
+  apiKey: conf.apiKey,
+  authDomain: conf.authDomain,
+  projectId: conf.projectId,
+  storageBucket: conf.storageBucket,
+  messagingSenderId: conf.messagingSenderId,
+  appId: conf.appId,
 };
 
 
@@ -21,3 +22,12 @@ export const db = getFirestore()
 export const storage = getStorage()
 
 
+
+
+
+// apiKey: "AIzaSyC6a_h50SvyOzcLaEZ0-HQci3ciJ_Aivko",
+// authDomain: "reactchat-818b8.firebaseapp.com",
+// projectId: "reactchat-818b8",
+// storageBucket: "reactchat-818b8.appspot.com",
+// messagingSenderId: "610253219706",
+// appId: "1:610253219706:web:d0ae48ee8fa9f036bf18c3"
